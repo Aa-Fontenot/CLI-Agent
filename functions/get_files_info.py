86f5072a7098a1550bec297cdd00e01582098ead
Shakashoon <fontenot.aaron3@gmail.com>
2026-01-20 23:14:07 -0600

    working on directory mapping functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,9 @@
+
+
 def get_file_info(working_directory, directory="."):
-    
+    absolute_workingdir_path = os.path.abspath(working_directory)
+    target_directory = os.path.normpath(os.path.join(absolute_workingdir_path, directory))
+    commonpath = os.path.commonpath([absolute_workingdir_path, target_directory]) == absolute_workingdir_path
+    if commonpath == False:
+        f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if 
